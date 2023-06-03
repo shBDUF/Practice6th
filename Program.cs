dcd3a6c30a66e601b0879676b17363858e554105
Grigoriy <inpuct@mail.ru>
2023-06-04 00:28:24 +0300

    3rd Ex

--- Program.cs
+++ Program.cs
@@ -23,3 +23,31 @@
 // double x = (b2-b1)/(k2-k1);
 // double y = k1*x+b1;
 // Console.WriteLine("Точка пересечения прямых - ({0};{1})",x,y);
+
+
+// Console.WriteLine("Введите число: ");
+// int inputNumber = Convert.ToInt32(Console.ReadLine());
+// int[] Fibbonachi = new int[2]{1,1};
+// int[] NegaFibbonachi= new int[2]{0, 1};
+// Array.Resize(ref Fibbonachi, inputNumber);
+// Array.Resize(ref NegaFibbonachi, inputNumber);
+// for (int i = 2; i < Fibbonachi.Length; i++)
+// {
+//     Fibbonachi[i] = Fibbonachi[i-1]+ Fibbonachi[i-2];
+// }
+// for (int i = 2; i < NegaFibbonachi.Length; i++)
+// {
+//     NegaFibbonachi[i] = NegaFibbonachi[i-2]- NegaFibbonachi[i-1];
+// }
+// Array.Reverse(NegaFibbonachi);
+// Console.WriteLine($"С числом {inputNumber} НегаФиббоначи получается - ");
+// ArrayOutput(NegaFibbonachi);
+// ArrayOutput(Fibbonachi);
+
+// void ArrayOutput(int[] massiv)
+// {
+//     for (int i = 0; i < massiv.Length; i++)
+//     {
+//         Console.Write($"{massiv[i]} ");
+//     }
+// }
