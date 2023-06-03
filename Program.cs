165bf0a4bfa9fdc4f9fa7918f79b4d6ea1e649ab
Grigoriy <inpuct@mail.ru>
2023-06-03 18:21:51 +0300

    2st Ex

--- Program.cs
+++ Program.cs
@@ -1,11 +1,25 @@
-﻿Console.WriteLine("Введите цифры - ");
-int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-int count = 0;
-for (int i = 0; i < array.Length; i++)
-{
-    if (array[i]>0)
-    {
-        count++;
-    }
-}
-Console.WriteLine("Количество цифр больше нуля -> {0}", count);
+﻿// Console.WriteLine("Введите цифры - ");
+// int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+// int count = 0;
+// for (int i = 0; i < array.Length; i++)
+// {
+//     if (array[i]>0)
+//     {
+//         count++;
+//     }
+// }
+// Console.WriteLine("Количество цифр больше нуля -> {0}", count);
+
+
+// Console.WriteLine("Введите b1: ");
+// double b1 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Введите k1: ");
+// double k1 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Введите b2: ");
+// double b2 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Введите k2: ");
+// double k2 = Convert.ToDouble(Console.ReadLine());
+
+// double x = (b2-b1)/(k2-k1);
+// double y = k1*x+b1;
+// Console.WriteLine("Точка пересечения прямых - ({0};{1})",x,y);
