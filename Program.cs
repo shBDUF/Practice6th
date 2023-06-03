4e084330bd970e8c304aadb9ef55387537f7a9a6
Grigoriy <inpuct@mail.ru>
2023-06-03 17:06:12 +0300

    1st Ex

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите цифры - ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i]>0)
+    {
+        count++;
+    }
+}
+Console.WriteLine("Количество цифр больше нуля -> {0}", count);
